249ae2216a2e9596544244a0e58fa4c58902d30f
Alex Perez-Davies <alexperezdavies@gmail.com>
2022-10-11 17:20:34 +0100

    Created database

---
+++ src/database/seed.sql
@@ -0,0 +1,19 @@
+BEGIN;
+
+INSERT INTO users VALUES
+  (1, 3, 'Ben', 'a@example.com', '$2a$12$A74tKw96m82AEZpJrIEQxecZGscKayJD/hD5/I6DuqKEJoQlAlNYO', '2017-12-25 00:00:00'),
+  (2, 1, 'Sara', 'b@example.com', '$2a$12$o2NP8ykiki.T11qRQOCnBeX68PhwXHprM/hxHkbAfqotWo2XT.vdW', '2017-12-25 00:00:00'),
+  (3, 4, 'Bea', 'c@example.com', '$2a$12$8HIQ9kqN0brBUfDigfSfB.9BlSsICw4LQ/tiw4U/zMW5QWtuTqMG6', '2017-12-25 00:00:00'),
+  (4, 2, 'Fin', 'd@example.com', '$2a$12$vpUb03L0WVHMfrHducL2SumogekzuPfh9Ych0Il8Zkt5Baqch7QZq', '2017-12-25 00:00:00')
+ON CONFLICT DO NOTHING;
+
+INSERT INTO pets VALUES
+  (1, 'Milo', 2, 'dog', '/images/dog.jpg', 1),
+  (2, 'Cupcake', 4, 'dolphin', '/images/cat.jpg', 1),
+  (3, 'Bao', 1, 'cat', '/images/cat.jpg', 0),
+  (4, 'Amber', 4, 'eagle', '/images/cat.jpg', 0),
+  (5, 'Mittens', 4, 'tiger', '/images/tiger.jpg', 0),
+  (6, 'Dumpling', 1, 'snake', '/images/snake.jpg', 0)
+ON CONFLICT DO NOTHING;
+
+COMMIT;
