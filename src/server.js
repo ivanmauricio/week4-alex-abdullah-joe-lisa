f82726644c71eb5060448a9a06197ddbe2a9de79
Alex Perez-Davies <alexperezdavies@gmail.com>
2022-10-11 17:38:21 +0100

    Exported server

--- src/server.js
+++ src/server.js
@@ -3,9 +3,10 @@ const cookieParser = require("cookie-parser");
 
 const staticHandler = express.static("public");
 
-
 const body = express.urlencoded({ extended: false });
 const cookies = cookieParser(process.env.COOKIE_SECRET);
 const server = express();
 server.use(staticHandler);
 server.use(cookies);
+
+module.exports = server;
