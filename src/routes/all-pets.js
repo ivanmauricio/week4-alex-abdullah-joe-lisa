a7cee88473a30607fb0a49b088ec9a6bf489e9f0
lisahns <lisa.hanes@hotmail.de>
2022-10-14 12:59:02 +0100

    reverted middleware

--- src/routes/all-pets.js
+++ src/routes/all-pets.js
@@ -3,6 +3,8 @@ const { getAllPets } = require("../model/pets.js");
 const { getSession } = require("../model/session.js");
 
 function get(req, res) {
+  const sid = req.signedCookies.sid;
+  const sessionId = getSession(sid)
   const currentUser = sessionId && sessionId.user_id;
   const petsList = getAllPets(); // Query from table
   const content = AllPets(petsList, currentUser); // HTML markup
